5ab69041bb18d3bc934cdacdad8d1086bd6d0c05
Marcos Fernández Pérez <marferp2@inf.upv.es>
2021-11-20 02:09:23 +0100

    tidal stuff moved to the specific class

--- playlist-reader.py
+++ playlist-reader.py
@@ -2,7 +2,10 @@ from types import prepare_class
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import config
+import tidal_backend
 import tidalapi
+import logging
+import json
 
 songs = []
 
@@ -13,25 +16,22 @@ sp = spotipy.Spotify(
     )
 )
 
-td = tidalapi.login(
-    user=config.tidal_user,
-    password=config.tidal_password
-)
-
-print(tidalapi.check_login())
-
-
-def print_playlist_songs(url):
+def playlist_songs(url):
     songs = sp.playlist_items(
         playlist_id=url, additional_types=('track',))['items']
-    for s in songs:
-        songs.append(s)
+    print(songs)
+    return songs
+
 
 
 def main():
-    print_playlist_songs(
-        url="https://open.spotify.com/playlist/7ozIozDp260fjNOZy1yzRG?si=6c9576eb3d3e46dd"
-    )
+    # songs = playlist_songs(
+    #     url="https://open.spotify.com/playlist/7ozIozDp260fjNOZy1yzRG?si=6c9576eb3d3e46dd"
+    # )
+
+    tidal = tidal_backend.TidalBackend()
+    tidal.oauth_session()
+
 
 
 if __name__ == "__main__":
